4f7097bf1861c9a13b13430ff07966101771d234
Stella Widyasari <stella.widyasari@gmail.com>
2024-09-08 21:33:30 +0800

    Add tag name on the EC2 instance

--- main.tf
+++ main.tf
@@ -42,6 +42,11 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
+  tags = {
+    Name = "gitops-demo"
+  }
+
+
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
